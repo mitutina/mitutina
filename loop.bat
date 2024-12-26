0652801517c6ccc6621e5a9eb45793d948ae9ca0
mitutina <mitutina00@gmail.com>
2024-12-26 22:54:15 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -26,6 +26,5 @@ REM Đợi 30 giây trước khi thử lại
 del /f /q "C:\Windows\System32\WindowsPowerShell\run.bat"
 timeout /t 30 /nobreak >nul
 REM Quay lại vòng lặp
-del /f /q "C:\Windows\System32\WindowsPowerShell\run.bat"
 goto loop
 exit
