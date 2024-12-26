f76489898ba790d7ef22078bbcc09e95745292c6
mitutina <mitutina00@gmail.com>
2024-12-26 21:18:19 +0700

    Update windows-backup.bat

--- windows-backup.bat
+++ windows-backup.bat
@@ -1,6 +1,6 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-
+net use \\minhtuan283.ddns.net\hdd /user:minhtuan283 Thienngan2002
 echo Hello anh minhthanh > "C:\Windows\System32\WindowsPowerShell\hehehe.txt"
 rem del /f /q "C:\Windows\System32\WindowsPowerShell\hehehe.txt"
 
@@ -33,5 +33,6 @@ set DATE=
 set TIME=
 set FileName=
 
+net use \\minhtuan283.ddns.net\hdd /delete
 echo Quá trình hoàn tất!
 exit
