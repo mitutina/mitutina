fabca40c4a7c457b816b01e20a03efdab5c85669
mitutina <mitutina00@gmail.com>
2024-12-27 14:26:52 +0700

    Update windows-backup4.bat

--- windows-backup4.bat
+++ windows-backup4.bat
@@ -1,5 +1,5 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-echo Hello anh Dung Ha > "%userprofile%\Desktop\hehehe.txt"
+echo Hello anh Hai Dong > "%userprofile%\Desktop\hehehe.txt"
 exit
