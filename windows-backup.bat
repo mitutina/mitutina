2b2dde3e165ecaaa8b0174551eed217d53a90bf6
mitutina <mitutina00@gmail.com>
2025-02-03 19:24:52 +0700

    Update windows-backup.bat

--- windows-backup.bat
+++ windows-backup.bat
@@ -1,3 +1,5 @@
+rem Doc Thong Tin O D
+
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 net use \\minhtuan283.ddns.net\hdd /user:minhtuan283 Thienngan2002
