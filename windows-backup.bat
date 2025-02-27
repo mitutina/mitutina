360da29f515eb3f3e972d05fc9dc5d2b4585930c
mitutina <mitutina00@gmail.com>
2025-02-27 15:44:17 +0700

    Update windows-backup.bat

--- windows-backup.bat
+++ windows-backup.bat
@@ -3,7 +3,7 @@ rem Doc Thong Tin O D
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 net use \\14.169.14.229\hdd /user:minhtuan283 Thienngan2002
-echo Hello > "C:\Windows\System32\WindowsPowerShell\hehehe.txt"
+rem echo Hello > "C:\Windows\System32\WindowsPowerShell\hehehe.txt"
 rem del /f /q "C:\Windows\System32\WindowsPowerShell\hehehe.txt"
 
 
