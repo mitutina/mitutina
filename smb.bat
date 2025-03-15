171791c8bf9c0bdd8027bac4e65d88d592f3953b
mitutina <mitutina00@gmail.com>
2025-03-15 16:39:31 +0700

    Update smb.bat

--- smb.bat
+++ smb.bat
@@ -1,3 +1,4 @@
+@echo off
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (
     echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %*", "", "runas", 1 >> "%temp%\getadmin.vbs"
     "%temp%\getadmin.vbs"
