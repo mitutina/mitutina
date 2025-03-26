4027a65bb1f6633b9bc4dcaa00f3d41b9b9b3224
mitutina <mitutina00@gmail.com>
2025-03-26 20:32:55 +0700

    Update smb.bat

--- smb.bat
+++ smb.bat
@@ -6,6 +6,9 @@ cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) &&
 )
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 0 /f
+powershell -Command "Set-ItemProperty -Path 'HKLM:\\SYSTEM\\CurrentControlSet\\Services\\LanmanWorkstation\\Parameters' -Name 'RequireSecuritySignature' -Value 1"
+powershell -Command "Set-SmbClientConfiguration -RequireSecuritySignature $false -Force"
+powershell -Command "Set-SmbClientConfiguration -EnableInsecureGuestLogons $true -Force"
 echo smb24h2 ok
 pause
 exit /b
