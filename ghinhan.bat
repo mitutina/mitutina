c7974ffb9e25f271e0b5916fde2d1043c9c2f83f
mitutina <mitutina00@gmail.com>
2025-06-03 16:06:56 +0700

    Update ghinhan.bat

--- ghinhan.bat
+++ ghinhan.bat
@@ -1,6 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
-
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "HideSCAMeetNow" /t REG_DWORD /d 1 /f
+powershell -Command "Set-ItemProperty -Path 'HKLM:\\SYSTEM\\CurrentControlSet\\Services\\LanmanWorkstation\\Parameters' -Name 'RequireSecuritySignature' -Value 1"
+powershell -Command "Set-SmbClientConfiguration -RequireSecuritySignature $false -Force"
+powershell -Command "Set-SmbClientConfiguration -EnableInsecureGuestLogons $true -Force"
 if not exist "F:\ghinhanhethong\" mkdir "F:\ghinhanhethong"
 
 :input_name
