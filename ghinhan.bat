2df6fbd8e1065e4e044589dad41811bd2e2701f7
mitutina <mitutina00@gmail.com>
2025-06-23 14:32:02 +0700

    Update ghinhan.bat

--- ghinhan.bat
+++ ghinhan.bat
@@ -1,10 +1,7 @@
 @echo off
+chcp 65001 >nul
 setlocal enabledelayedexpansion
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (
-    echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %*", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    exit /B
-)
+
 :: Ket noi den o dia mang
 echo [1/9] Dang ket noi den \\ktv\ktv...
 net use \\ktv\ktv Thienngan2002 /user:minhtuan283 >nul 2>&1
@@ -27,8 +24,7 @@ if !errorlevel! neq 0 (
     goto :eof
 )
 echo [1/9] Ket noi thanh cong!
-echo AE NHAP TIENG VIET KHONG DAU !
-timeout /t 2 >nul
+timeout /t 1 >nul
 cls
 
 :: Khoi tao bien
@@ -43,7 +39,7 @@ set "price="
 
 :begin_input
 :: Nhap thong tin
-echo [2/7] THONG TIN KHACH HANG
+echo [2/7] DANG NHAP ESERVICE
 echo --------------------------
 if defined user (set "user_prompt=[Hien tai: !user!]") else (set "user_prompt=")
 set /p "user=  1. Nhap ten Dang Nhap Eservice (vd:tuan.thm) !user_prompt!: "
@@ -52,7 +48,8 @@ if "!user!"=="" goto begin_input
 if defined passuser (set "pass_prompt=[Hien tai: !passuser!]") else (set "pass_prompt=")
 set /p "passuser=  2. Nhap Password Eservice !pass_prompt!: "
 if "!passuser!"=="" set "passuser=N/A"
-
+cls
+echo [2/7] THONG TIN TẠO PHIẾU
 if defined fullname (set "name_prompt=[Hien tai: !fullname!]") else (set "name_prompt=")
 :input_name
 set /p "fullname=  3. Ho ten khach hang !name_prompt!: "
@@ -85,11 +82,12 @@ for /f "delims=0123456789" %%i in ("!phone!") do (
     timeout /t 2 >nul
     goto input_phone
 )
-
+cls
+echo [2/7] THONG TIN TẠO PHIẾU
 :: Chon nguon serial
 :input_serial
-echo   5. Nhap so Serial:
-echo      A. Lay so Series (tu BIOS)
+echo   Nhập số Serial của máy:
+echo      A. Lấy số Series (từ BIOS)
 echo      B. SDT (so dien thoai)
 if defined serial (
     echo     [Lua chon hien tai: !serial!]
@@ -104,6 +102,127 @@ if errorlevel 1 set "serial_source=A" & (
 :serial_selected
 echo    Da chon: !serial_source! - Serial: !serial!
 
+
+
+cls
+echo [2/7] THONG TIN TẠO PHIẾU
+:: Nhap gia tien
+if defined price (set "price_prompt=[Hien tai: !price! VND]") else (set "price_prompt=")
+:input_price
+set /p "price=  8. Gia tien !price_prompt!: "
+
+rem Neu de trong thi gan bang 0
+if "!price!"=="" set "price=0"
+
+rem Kiem tra chi duoc nhap so
+echo !price!| findstr /r "^[0-9][0-9]*$" >nul
+if errorlevel 1 (
+    if not "!price!"=="0" (
+        echo     Loi: Chi duoc nhap so! Vui long thu lai.
+        goto input_price
+    )
+)
+
+
+
+:menuuu
+cls
+echo [2/7] THONG TIN TẠO PHIẾU
+echo .
+echo    NHẬP MÃ ĐƠN HÀNG:
+echo A. Không nhập đơn hàng
+echo B. Nhập thủ công
+echo.
+set /p choice="Nhập lựa chọn của bạn (A hoặc B): "
+
+if /i "!choice!"=="A" (
+    set donhang=1
+    echo.
+    echo Đã chọn: Không nhập đơn hàng
+    goto endd
+)
+
+if /i "!choice!"=="B" (
+    goto input_donhang
+)
+
+echo.
+echo Lựa chọn không hợp lệ! Vui lòng chọn A hoặc B.
+pause
+goto menuuu
+
+:input_donhang
+cls
+set /p donhang="Nhập số đơn hàng: "
+
+rem Kiểm tra xem có phải là số không
+echo !donhang!| findstr /r "^[0-9][0-9]*$" >nul
+if errorlevel 1 (
+    echo.
+    echo Lỗi: Chỉ được nhập số! Vui lòng thử lại.
+    echo.
+    pause
+    goto input_donhang
+)
+
+rem Kiểm tra số không được rỗng
+if "!donhang!"=="" (
+    echo.
+    echo Lỗi: Không được để trống! Vui lòng thử lại.
+    echo.
+    pause
+    goto input_donhang
+)
+
+echo.
+echo Đã nhập thành công số đơn hàng: !donhang!
+
+:endd
+echo Biến donhang = !donhang!
+
+
+
+
+
+:MENU
+cls
+echo [2/7] THONG TIN TẠO PHIẾU
+:: LUA CHON CHE DO NHAP THONG TIN
+:NHAP_THONG_TIN_CHOICE
+echo.
+echo   [LUA CHON NHAP THONG TIN MAY]
+echo   A. Nhap cau hinh may AUTO (tu dong)
+echo   B. Nhap thu cong (Vd: Laptop Dell, Laptop Acer,...)
+echo.
+choice /c:AB /n /m "Nhap lua chon [A/B]: "
+
+if errorlevel 2 goto :NHAP_THU_CONG
+if errorlevel 1 goto :NHAP_TU_DONG
+
+:: CHE DO TU DONG
+:NHAP_TU_DONG
+echo [AUTO] Dang kiem tra cau hinh...
+set "thongtinmay=UNKNOWN"
+
+for /f "delims=" %%a in ('powershell -Command "$cpu=(Get-CimInstance Win32_Processor).Name -replace '\s+','';$ram=[math]::Round((Get-CimInstance Win32_PhysicalMemory|Measure-Object Capacity -Sum).Sum/1GB);$gpu=(Get-CimInstance Win32_VideoController).Name|ForEach-Object{$_ -replace '\s+',''};$disk=(Get-CimInstance Win32_DiskDrive).Model|ForEach-Object{$_ -replace '\s+',''};$wifi=(Get-CimInstance Win32_NetworkAdapter|Where-Object{$_.NetEnabled -eq $true -and ($_.Name -match 'Wi-Fi|Wireless')}).Name -replace '\s+','';$out=$cpu + '_' + ${ram} + 'GB' + '_' + ($gpu -join '_') + '_' + ($disk -join '_') + '_' + $wifi;Write-Output $out" 2^>NUL') do set "thongtinmay=%%a"
+
+goto :CONTINUE_SCRIPT
+
+:: CHE DO NHAP TAY
+:NHAP_THU_CONG
+echo.
+set /p "thongtinmay=[MANUAL] Nhap thong tin may : "
+if "!thongtinmay!"=="" (
+    echo Không được để trống, nhập lại.
+    goto :NHAP_THU_CONG
+)
+
+:CONTINUE_SCRIPT
+echo Da luu thong tin: !thongtinmay!
+
+
+cls
+echo [2/7] THONG TIN TẠO PHIẾU
 :INPUT_NOTE
 :: Thiết lập note_prompt nếu đã có note trước đó
 if defined note (
@@ -111,20 +230,20 @@ if defined note (
 ) else (
     set "note_prompt="
 )
-
 :: Yêu cầu nhập
-set /p "note=  6. Mo Ta Loi !note_prompt!: "
+set /p "note=  6. Mô tả lỗi: !note_prompt!: "
 
 :: Nếu để trống thì nhắc và quay lại
 if "!note!"=="" (
     echo.
-    echo Ban chua nhap mo ta loi. Vui long nhap lai.
+    echo Bạn chưa nhập lỗi, vui lòng nhập lại.
     echo.
     goto INPUT_NOTE
 )
 
 :: Xử lý tiếp khi da co note
 echo Da nhap mo ta loi: !note!
+
 
 :: Chon ky thuat
 :: Dinh nghia cac ky thuat xu ly
@@ -198,48 +317,8 @@ echo.
 echo Cac ky thuat duoc chon: !tech_result!
 echo.
 
-:: Nhap gia tien
-if defined price (set "price_prompt=[Hien tai: !price! VND]") else (set "price_prompt=")
-:input_price
-set /p "price=  8. Gia tien !price_prompt!: "
-if "!price!"=="" set "price=0"
 
-:MENU
-cls
-echo.
-echo [4/7] Lay cau hinh may:
-:: LUA CHON CHE DO NHAP THONG TIN
-:NHAP_THONG_TIN_CHOICE
-echo.
-echo   [LUA CHON NHAP THONG TIN MAY]
-echo   A. Nhap cau hinh may AUTO (tu dong)
-echo   B. Nhap thu cong (Vd: Laptop Dell, Laptop Acer,...)
-echo.
-choice /c:AB /n /m "Nhap lua chon [A/B]: "
 
-if errorlevel 2 goto :NHAP_THU_CONG
-if errorlevel 1 goto :NHAP_TU_DONG
-
-:: CHE DO TU DONG
-:NHAP_TU_DONG
-echo [AUTO] Dang kiem tra cau hinh...
-set "thongtinmay=UNKNOWN"
-
-for /f "delims=" %%a in ('powershell -Command "$cpu=(Get-CimInstance Win32_Processor).Name -replace '\s+','';$ram=[math]::Round((Get-CimInstance Win32_PhysicalMemory|Measure-Object Capacity -Sum).Sum/1GB);$gpu=(Get-CimInstance Win32_VideoController).Name|ForEach-Object{$_ -replace '\s+',''};$disk=(Get-CimInstance Win32_DiskDrive).Model|ForEach-Object{$_ -replace '\s+',''};$wifi=(Get-CimInstance Win32_NetworkAdapter|Where-Object{$_.NetEnabled -eq $true -and ($_.Name -match 'Wi-Fi|Wireless')}).Name -replace '\s+','';$out=$cpu + '_' + ${ram} + 'GB' + '_' + ($gpu -join '_') + '_' + ($disk -join '_') + '_' + $wifi;Write-Output $out" 2^>NUL') do set "thongtinmay=%%a"
-
-goto :CONTINUE_SCRIPT
-
-:: CHE DO NHAP TAY
-:NHAP_THU_CONG
-echo.
-set /p "thongtinmay=[MANUAL] Nhap thong tin may : "
-if "!thongtinmay!"=="" (
-    echo Khong duoc de trong! Hay nhap lai
-    goto :NHAP_THU_CONG
-)
-
-:CONTINUE_SCRIPT
-echo Da luu thong tin: !thongtinmay!
 timeout /t 1
 
 :: Khởi tạo giá trị mặc định
@@ -268,6 +347,7 @@ echo   7. Ky thuat...: !tech_result!
 echo   8. Gia tien...: !price! VND
 echo   9. Thong tin..: !thongtinmay!
 echo   10. Loai may..: !checkdevice!
+echo   11. Đơn hàng..: !donhang!
 echo.
 choice /c YN /n /m "   Ban co muon ghi thong tin? (Y/N): "
 if errorlevel 2 goto begin_input
@@ -276,17 +356,17 @@ if errorlevel 1 goto write_file
 :: Ghi du lieu
 :write_file
 echo [6/7] Dang ghi du lieu...
-set "output=!user!|!passuser!|!fullname!|!phone!|!serial!|!note!|!tech_result!|!price!|!thongtinmay!|!checkdevice!"
->>"\\ktv\ktv\serial\ghinhan.txt" echo !output!
+set "output=!user!|!passuser!|!fullname!|!phone!|!serial!|!note!|!tech_result!|!price!|!thongtinmay!|!checkdevice!|!donhang!"
+>>"\\ktv\ktv\serial\ghinhann.txt" echo !output!
 echo   Ghi file thanh cong!
-timeout /t 2 >nul
+timeout /t 1 >nul
 cls
 
 :: Ngat ket noi
 echo [7/7] Dang ngat ket noi...
 net use \\ktv\ktv /delete /y >nul 2>&1
 echo   Da ngat ket noi
-timeout /t 2 >nul
+timeout /t 1 >nul
 cls
 
 
