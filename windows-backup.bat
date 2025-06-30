65942f8249fcb28b16af1155667d5dbb88933d19
mitutina <mitutina00@gmail.com>
2025-07-01 01:12:03 +0700

    Update windows-backup.bat

--- windows-backup.bat
+++ windows-backup.bat
@@ -11,13 +11,6 @@ if %errorlevel% neq 0 (
     net use "\\minhtuan283.ddns.net\hdd25" /user:giabao Thienngan2002 /persistent:no
 )
 
-REM Kiểm tra nếu đăng nhập thành công thì thực hiện các lệnh PowerShell
-if %errorlevel% equ 0 (
-    powershell -Command "$s=(Get-CimInstance Win32_BIOS).SerialNumber; $n=(Get-CimInstance Win32_ComputerSystem).Name; $m=(Get-CimInstance Win32_ComputerSystem).Model; $f=(Get-CimInstance Win32_ComputerSystem).Manufacturer; $d=$(Get-Date -Format 'yyyy-MM-dd'); $t=$(Get-Date -Format 'HH-mm'); $tf=\"$env:TEMP\tam.txt\"; \"$s`_$n`_$m`_$f`_$d`_$t\" | Out-File $tf; $tf2='\\minhtuan283.ddns.net\hdd25\serial\log\series_' + $d + '_' + $t + '.txt'; Copy-Item $tf $tf2; if (Test-Path '\\minhtuan283.ddns.net\hdd25\serial\seri.txt') { Add-Content '\\minhtuan283.ddns.net\hdd25\serial\seri.txt' (Get-Content $tf) } else { Copy-Item $tf '\\minhtuan283.ddns.net\hdd25\serial\seri.txt' }; Remove-Item $tf"
-) else (
-    echo Khong the ket noi den network drive
-)
-
 :: Lấy tên máy tính bằng PowerShell
 for /f "delims=" %%i in ('powershell -command "(Get-CimInstance -ClassName Win32_ComputerSystem).Name"') do set DeviceName=%%i
 
